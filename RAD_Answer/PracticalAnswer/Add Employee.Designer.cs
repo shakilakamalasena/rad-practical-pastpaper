8af3c8448c4e93c1782c8d7bf946b4664a66809c
Shakila Kamalasena <104765058+shakilakamalasena@users.noreply.github.com>
2024-10-06 23:23:38 +0530

    Added UI only

---
+++ RAD_Answer/PracticalAnswer/Add Employee.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace PracticalAnswer
+{
+    partial class Add_Employee
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label2 = new Label();
+            textBox3 = new TextBox();
+            label3 = new Label();
+            label4 = new Label();
+            textBox5 = new TextBox();
+            label5 = new Label();
+            checkBox1 = new CheckBox();
+            checkBox2 = new CheckBox();
+            button1 = new Button();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(62, 85);
+            label1.Name = "label1";
+            label1.Size = new Size(124, 21);
+            label1.TabIndex = 0;
+            label1.Text = "Employee Name";
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(218, 82);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(303, 29);
+            textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox2.Location = new Point(218, 138);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(303, 29);
+            textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(62, 141);
+            label2.Name = "label2";
+            label2.Size = new Size(154, 21);
+            label2.TabIndex = 2;
+            label2.Text = "Destination/ Position";
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox3.Location = new Point(218, 194);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(303, 29);
+            textBox3.TabIndex = 5;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(62, 197);
+            label3.Name = "label3";
+            label3.Size = new Size(92, 21);
+            label3.TabIndex = 4;
+            label3.Text = "Basic Salary";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.Location = new Point(62, 256);
+            label4.Name = "label4";
+            label4.Size = new Size(111, 21);
+            label4.TabIndex = 6;
+            label4.Text = "Staff Transport";
+            // 
+            // textBox5
+            // 
+            textBox5.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox5.Location = new Point(218, 312);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(303, 29);
+            textBox5.TabIndex = 9;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label5.Location = new Point(62, 315);
+            label5.Name = "label5";
+            label5.Size = new Size(116, 21);
+            label5.TabIndex = 8;
+            label5.Text = "Allowance Rate";
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(218, 260);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(44, 19);
+            checkBox1.TabIndex = 10;
+            checkBox1.Text = "use";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Location = new Point(339, 256);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(82, 19);
+            checkBox2.TabIndex = 11;
+            checkBox2.Text = "do not use";
+            checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(454, 422);
+            button1.Name = "button1";
+            button1.Size = new Size(128, 38);
+            button1.TabIndex = 12;
+            button1.Text = "Back to Home";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(293, 422);
+            button2.Name = "button2";
+            button2.Size = new Size(128, 38);
+            button2.TabIndex = 13;
+            button2.Text = "Add Employee";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // Add_Employee
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(611, 486);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(checkBox2);
+            Controls.Add(checkBox1);
+            Controls.Add(textBox5);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(textBox3);
+            Controls.Add(label3);
+            Controls.Add(textBox2);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Name = "Add_Employee";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Add_Employee";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label2;
+        private TextBox textBox3;
+        private Label label3;
+        private Label label4;
+        private TextBox textBox5;
+        private Label label5;
+        private CheckBox checkBox1;
+        private CheckBox checkBox2;
+        private Button button1;
+        private Button button2;
+    }
+}
---
+++ RAD_Answer/PracticalAnswer/Home.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PracticalAnswer
+{
+    public partial class Home : Form
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_enterData_Click(object sender, EventArgs e)
+        {
+            Add_Employee add_Employee = new Add_Employee();
+            add_Employee.Show();
+            this.Hide();
+        }
+
+        private void btn_viewSalary_Click(object sender, EventArgs e)
+        {
+            View_Salary view_Salary = new View_Salary();
+            view_Salary.Show();
+            this.Hide();
+        }
+    }
+}
